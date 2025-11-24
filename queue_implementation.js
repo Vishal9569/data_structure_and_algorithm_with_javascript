84bbc61e375212375fc9f0724c7764772040910e
vishal001-dynamic <vk96431@gmail.com>
2025-11-24 18:31:41 +0530

    add queue

---
+++ queue_implementation.js
@@ -0,0 +1,8 @@
+//  Queue is linear data-structure that push is backend side and pop in fortend side that follow FIFO structure // 
+
+class Queue{
+     
+    constructor(){
+        this.items = [];
+    }
+}

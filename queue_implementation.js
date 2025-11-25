9958be941235a7fbee6332f201d6da34c7eb5301
Vishal kumar <149870754+Vishal9569@users.noreply.github.com>
2025-11-25 18:26:52 +0530

    Add queue implementation with basic operations
    Implemented queue methods: enqueue, dequeue, isEmpty, peek, size, and print queue.

--- queue_implementation.js
+++ queue_implementation.js
@@ -1,3 +1,5 @@
+
+
 //  Queue is linear data-structure that push is backend side and pop in fortend side that follow FIFO structure // 
 
 class Queue{
@@ -5,4 +7,56 @@ class Queue{
     constructor(){
         this.items = [];
     }
+
+    enqueue(val){
+        this.items.push(val);
+    }
+
+    dequeue(){
+        return this.isEmtpy() ? "queue is empty" : this.items.shift();
+    }
+
+    isEmtpy(){
+        return this.items.length === 0;
+    }
+
+    peek(){
+        if(this.isEmtpy()) {return -1;}
+        return this.items[0];
+    }
+
+    size(){
+        return this.items.length;
+    }
+
+    prtQue()   { console.log(this.items);
+    }
 }
+
+let obj = new Queue();
+obj.enqueue(2);
+obj.enqueue(3);
+obj.enqueue(4);
+
+console.log(obj.peek())
+console.log(obj.size())
+console.log(obj.isEmtpy());
+obj.prtQue();
+
+obj.dequeue();
+
+console.log(obj.peek())
+console.log(obj.size())
+console.log(obj.isEmtpy());
+
+obj.prtQue();
+
+obj.dequeue();
+obj.dequeue();
+
+
+console.log(obj.peek())
+console.log(obj.size())
+console.log(obj.isEmtpy());
+
+obj.prtQue();

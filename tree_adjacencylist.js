b7b1676df15cd9aece193510814ca9fc0d904330
Vishal kumar <vk965431@gmail.com>
2025-11-29 17:53:07 +0530

    add code

--- tree_adjacencylist.js
+++ tree_adjacencylist.js
@@ -1,10 +1,15 @@
  const node = 5;
- let edge = [[0,2] , [1,2] , [1,3] , [2,4]];
+ let adj = [[0,2] , [1,2] , [1,3] , [2,4]];
 
- const arr = Array.from({length:n} , ()  => [] );
+ const arr = Array.from({length: node} , ()  => [] );
 
- for( let [u,v] of edge){
+ for( let [u,v] of adj){
      
-    ad
+    arr[u].push(v);
+    arr[v].push(u);
  }
  
+console.log("this is edge : " , adj);  // this edges 
+console.log("this is node " , arr);  // this is node 
+
+

fbb9a361bf08d56ff0382712c3da13522d546b34
vishal001-dynamic <vk96431@gmail.com>
2025-12-01 18:04:56 +0530

    add strings data structure

---
+++ strings.js
@@ -0,0 +1,33 @@
+let str = ["vishal", "kumar", "kushwaha"];
+
+let str1 = "  vishal KUMAR  ";
+
+console.log(str.length);
+
+console.log(str[0]);
+
+console.log(str1.charAt(0));
+
+console.log(str.slice(1, 2));
+
+console.log(str.includes("kumar"));
+
+console.log(str.indexOf("kuswhaha"));
+
+console.log(str.lastIndexOf("kushwaha"));
+
+console.log(str1.toLowerCase());
+
+console.log(str1.toUpperCase());
+
+console.log(str1.trim());
+
+console.log(str1.split(""));
+
+// console.log(str.replace("vishal", "suraj"));
+
+// console.log(str.repeat("kumar"));
+
+// console.log(str.startsWith("vishal"));
+
+// console.log(str.endsWith("kumar"));
